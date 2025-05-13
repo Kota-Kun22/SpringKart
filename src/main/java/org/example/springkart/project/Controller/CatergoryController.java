40a275a89b9e41e3e615d13f9d23be1d0daab7e6
Harsh Rastogi <68362699+Kota-Kun22@users.noreply.github.com>
2025-05-13 11:33:20 +0530

    for this commit we have done everything in the Controller only  like getting the category and adding in category

--- src/main/java/org/example/springkart/project/Controller/CatergoryController.java
+++ src/main/java/org/example/springkart/project/Controller/CatergoryController.java
@@ -1,4 +1,33 @@
 package org.example.springkart.project.Controller;
 
+
+import org.example.springkart.project.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class CatergoryController {
+
+    private List<Category> categoryList= new ArrayList<>();
+
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategory(){
+        return categoryList;
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategory( @RequestBody Category category){
+        categoryList.add(category);
+        System.out.println(categoryList);
+        return "Category Created Successfully";
+    }
+
+
+
 }
--- src/main/java/org/example/springkart/project/model/Category.java
+++ src/main/java/org/example/springkart/project/model/Category.java
@@ -1,4 +1,28 @@
 package org.example.springkart.project.model;
 
 public class Category {
+
+    private Long CategoryId;
+    private String CategoryName;
+
+    public Category(String categoryName, Long categoryId) {
+        CategoryName = categoryName;
+        CategoryId = categoryId;
+    }
+
+    public Long getCategoryId() {
+        return CategoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        CategoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return CategoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        CategoryName = categoryName;
+    }
 }

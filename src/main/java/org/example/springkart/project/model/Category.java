4c446a3f8cc2f6069ad0baef6a2309e90e822f98
Harsh Rastogi <68362699+Kota-Kun22@users.noreply.github.com>
2025-05-19 17:43:07 +0530

    Lombok is configured!

--- src/main/java/org/example/springkart/project/model/Category.java
+++ src/main/java/org/example/springkart/project/model/Category.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name="Categories_Table")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
 
     @Id
@@ -13,32 +19,5 @@ public class Category {
     private Long categoryId;
     private String categoryName;
 
-    public Category() {
-        //this one here is for JPA
-    }
 
-    public Category(String categoryName, Long categoryId) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
